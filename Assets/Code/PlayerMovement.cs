edf47bae75181a5b25de88082e79f142a22ac5ae
Rudi <rudi.rutanen@hotmail.com>
2021-01-30 16:08:01 +0200

    First implementation of movement and boundaries
    You can edit the speed and boundary coordinates straight from the editor.

---
+++ Assets/Code/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    void Awake()
+    {
+        speed = 5f;
+    }
+
+    private void Update()
+    {
+        float moveX = 0f;
+        float moveY = 0f;
+
+        
+        if (Input.GetKey(KeyCode.W)) {
+            moveY = +1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveX = -1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveY = -1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveX = +1f;
+        }
+        
+        Vector3 moveDir = new Vector3(moveX, moveY).normalized;
+        transform.position += moveDir * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/WASD_Movement.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class WASD_Movement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float boundaryTop;
+    [SerializeField]
+    private float boundaryDown;
+    [SerializeField]
+    private float boundaryLeft;
+    [SerializeField]
+    private float boundaryRight;
+
+    void Awake()
+    {
+        speed = 5f;
+        boundaryTop = 5.5f;
+        boundaryLeft = -5f;
+        boundaryDown = -5.5f;
+        boundaryRight = 5f;
+    }
+
+    private void Update()
+    {
+        float moveX = 0f;
+        float moveY = 0f;
+
+        if (Input.GetKey(KeyCode.W) && transform.position.y < boundaryTop)
+        {
+            moveY = +1f;
+        }
+        if (Input.GetKey(KeyCode.A) && transform.position.x > boundaryLeft)
+        {
+            moveX = -1f;
+        }
+        if (Input.GetKey(KeyCode.S) && transform.position.y > boundaryDown)
+        {
+            moveY = -1f;
+        }
+        if (Input.GetKey(KeyCode.D) && transform.position.x < boundaryRight)
+        {
+            moveX = +1f;
+        }
+
+        Debug.Log(transform.position);
+        Vector3 moveDir = new Vector3(moveX, moveY).normalized;
+        transform.position += moveDir * speed * Time.deltaTime;
+    }
+}
